e35c103adab1022d1db82abe6bf764fc47ceaabe
Alessandro <personal@alessandrozolli.com>
2023-01-23 11:23:52 +0100

    Manteinance

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
     import "../style/app.css";
 </script>
 
-<div class="bg-black text-white p-5 mx-auto">
+<div class="bg-black text-white p-5 mx-auto max-w-6xl">
     <slot /> 
 </div>
---
+++ src/routes/langicon.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let imgPath: string;
+</script>
+
+<img src={imgPath} alt={imgPath.split("/")[1].split(".")[0]} class="mb-4 mx-3 w-[45px] object-contain">
